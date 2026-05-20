2484276e0d2031845904eaea3659cf7957fd8c66
moizpatrawala1-pixel <moizpatrawala1@gmail.com>
2026-05-20 13:46:37 +0530

    Implement service worker caching for assets

--- sw.js
+++ sw.js
@@ -1,3 +1,4 @@
-self.addEventListener('fetch', (e) => {
-  e.respondWith(caches.match(e.request).then((r) => r || fetch(e.request)));
-});
+const CACHE='cfa-l2-v1';
+const ASSETS=['/','/index.html','/questions.json','/manifest.json'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
